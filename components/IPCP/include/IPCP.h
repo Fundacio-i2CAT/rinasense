6e08a69b474200f7c069b7bf5b245608ad432eb0
François-Denis Gonthier <francois-denis@trianetworksystems.com>
2022-06-17 18:24:42 -0400

    Move IPCP events enum and structure to is own file
    Porting "NetworkInterface" from FreeRTOS to POSIX. Make sure we can #include the
    IPCP event definitions without including things that are specific to FreeRTOS.

--- components/IPCP/include/IPCP.h
+++ components/IPCP/include/IPCP.h
@@ -23,34 +23,6 @@ typedef struct xQUEUE_FIFO
 
 } rfifo_t;
 
-typedef enum RINA_EVENTS
-{
-    eNoEvent = -1,
-    eNetworkDownEvent,          /* 0: The network interface has been lost and/or needs [re]connecting. */
-    eNetworkRxEvent,            /* 1: The network interface has queued a received Ethernet frame. */
-    eNetworkTxEvent,            /* 2: Let the Shim-task send a network packet. */
-    eShimEnrolledEvent,         /* 3: Shim Enrolled: network Interface Init*/
-    eARPTimerEvent,             /* 4: The ARP timer expired. */
-    eStackTxEvent,              /* 5: The software stack IPCP has queued a packet to transmit. */
-    eEFCPTimerEvent,            /* 6: See if any IPCP socket needs attention. */
-    eEFCPAcceptEvent,           /* 7: Client API FreeRTOS_accept() waiting for client connections. */
-    eShimFlowAllocatedEvent,    /* 8: A flow has been allocated on the shimWiFi*/
-    eStackFlowAllocateEvent,    /*9: The Software stack IPCP has received a Flow allocate request. */
-    eStackAppRegistrationEvent, /*10: The Software stack IPCP has received a AppRegistration Event*/
-    eFactoryInitEvent,          /*11: The IPCP factories has been initialized. */
-    eShimAppRegisteredEvent,    /* 12: The Normal IPCP has been registered into the Shim*/
-    eSendMgmtEvent,             /* 13: Send Mgmt PDU */
-
-} eRINAEvent_t;
-
-/**
- * Structure for the information of the commands issued to the RINA task.
- */
-typedef struct xRINA_TASK_COMMANDS
-{
-    eRINAEvent_t eEventType; /**< The event-type enum */
-    void *pvData;            /**< The data in the event */
-} RINAStackEvent_t;
 
 // typedef uint16_t ipcProcessId_t;
 
