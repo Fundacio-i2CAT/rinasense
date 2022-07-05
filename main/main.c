3323b82bb53c6a10157fd570cb2ad0b0a22e3753
esmaxness <dfsarabia@gmail.com>
2022-07-05 13:55:18 +0200

    Setting sensor DHT22 and adding into the sensor app

--- main/main.c
+++ main/main.c
@@ -11,6 +11,8 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 
+#include "dht.h"
+
 #define TAG_APP "[Sensor-APP]"
 
 void app_main(void)
@@ -29,26 +31,43 @@ void app_main(void)
 	struct rinaFlowSpec_t *xFlowSpec = pvPortMalloc(sizeof(*xFlowSpec));
 	uint8_t Flags = 1;
 	int i = 0;
-	char temperature[17];
+	char json[200];
+	float Humidity;
+	float Temperature;
 
+	gpio_set_pull_mode(GPIO_NUM_4, GPIO_PULLUP_ONLY);
 	vTaskDelay(1000);
 
 	ESP_LOGI(TAG_APP, "----------- Requesting a Flow ----- ");
 
-	xAppPortId = RINA_flow_alloc("mobile.DIF", "Temperature", "sensor", xFlowSpec, Flags);
+	xAppPortId = RINA_flow_alloc("mobile.DIF", "STH1", "sensor", xFlowSpec, Flags);
 
 	ESP_LOGI(TAG_APP, "Flow Port id: %d ", xAppPortId);
 	if (xAppPortId != -1)
 	{
 
-		while (i < 10)
+		while (i < 100)
 		{
-			sprintf(temperature, "Temperature:30C\n");
 
-			if (RINA_flow_write(xAppPortId, (void *)temperature, strlen(temperature)))
+			if (dht_read_float_data(DHT_TYPE_AM2301, GPIO_NUM_4,
+									&Humidity, &Temperature) != ESP_OK)
 			{
-				ESP_LOGI(TAG_APP, "Sent Data: %s successfully", temperature);
+
+				ESP_LOGI(TAG_APP, "Error to read dht");
 			}
+			ESP_LOGI(TAG_APP, "Temperature: %.1f C", Temperature);
+			ESP_LOGI(TAG_APP, "Humidity: %.1f%% ", Humidity);
+			sprintf(json, "{\n timeStamp: 1111111111,\n sensorId: STH1, \n sensorType: DHT22, \n data:{ \n\t Temperature: %.1f C, \n\t Humidity: %.1f%% \n }\n}\n",
+					Temperature,
+					Humidity);
+
+			if (RINA_flow_write(xAppPortId, (void *)json, strlen(json)))
+			{
+				ESP_LOGI(TAG_APP, "Sent Data successfully");
+			}
+
+			vTaskDelay(8000 / portTICK_RATE_MS);
+
 			i = i + 1;
 		}
 	}
