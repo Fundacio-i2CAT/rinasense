6b30b60b0c3cdc9ab772c189678dae8566bb4880
Steve Bunch <steve.bunch@gmail.com>
2022-09-19 11:09:36 -0400

    Fix argument to pxGetNetworkBufferWithDescriptor

--- components/NetworkInterface/esp32/NetworkInterface.c
+++ components/NetworkInterface/esp32/NetworkInterface.c
@@ -291,7 +291,6 @@ esp_err_t xNetworkInterfaceInput(void *buffer, uint16_t len, void *eb)
 {
 	NetworkBufferDescriptor_t *pxNetworkBuffer;
 	const TickType_t xDescriptorWaitTime = pdMS_TO_TICKS(250);
-    struct timespec ts;
 	RINAStackEvent_t xRxEvent = {
         .eEventType = eNetworkRxEvent,
         .xData.PV = NULL
@@ -304,10 +303,7 @@ esp_err_t xNetworkInterfaceInput(void *buffer, uint16_t len, void *eb)
 		return ESP_OK;
 	}
 
-    if (!rstime_waitmsec(&ts, 250))
-        return ESP_FAIL;
-
-    pxNetworkBuffer = pxGetNetworkBufferWithDescriptor(len, &ts);
+    pxNetworkBuffer = pxGetNetworkBufferWithDescriptor(len, 250 * 1000);
 	// ESP_LOGE(TAG_WIFI,"xNetworkInterfaceInput Taking buffer to copy wifidriver buffer");
 
 	if (pxNetworkBuffer != NULL)
